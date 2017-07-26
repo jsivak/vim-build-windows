94dbe8a30c3d14b77d5151c246509e81e9a41943
Prabir Shrestha <mail@prabir.me>
2017-07-26 00:34:30 -0700

    reorder flags

--- configure.cmd
+++ configure.cmd
@@ -8,30 +8,32 @@ set VS_DIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0"
 
 :: Target architecture, AMD64 (64-bit) or I386 (32-bit)
 set CPU=AMD64
+:: set CPU=I386
 
 :: Toolchain, x86_amd64 (cross-compile 64-bit) or x86 (32-bit) or amd64 (64-bit)
 set TOOLCHAIN=x86_amd64
+:: set TOOLCHAIN=x86
 
 :: TINY, SMALL, NORMAL, BIG or HUGE. NORMAL or above recommended
 set FEATURES=HUGE
 
 :: yes for gvim, no for vim
-set GUI=no
-set OLE=yes
 set DIRECTX=yes
+set OLE=yes
 set IME=yes
 set CSCOPE=yes
+set ICONV=yes
+set DEBUG=no
+set TERMGUICOLORS=yes
+set TERMINAL=yes
+set CHANNEL=yes
+
+set NETBEANS=yes
+set NBDEBUG=no
 
 set DYNAMIC_PERL=yes
 set PERL=C:\perl64
 set PERL_VER=524
-
-set NETBEANS=yes
-
-:: UTF-8 encoding
-set MBYTE=yes
-set ICONV=yes
-set DEBUG=no
 
 :: Enable Python scripting
 set DYNAMIC_PYTHON=yes
@@ -52,8 +54,6 @@ set TCL=C:\Tcl
 :set DYNAMIC_RUBY=no
 :set RUBY=C:\Ruby22-x64
 :set RUBY_MSVCRT_NAME=msvcrt
-
-set TERMINAL=yes
 
 set WINVER=0x500
 
